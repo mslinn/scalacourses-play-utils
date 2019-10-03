0080ff8777f59c01c82b4557b5f2cadcca5d65e3
Mike Slinn <mslinn@micronauticsresearch.com>
2019-10-03 11:28:20 -0700

    -

--- src/main/scala-2.13/views/html/data/hidden.scala
+++
@@ -1,9 +0,0 @@
-package views.html.data
-
-import play.api.data.Field
-import play.twirl.api.Html
-
-object hidden {
-  def apply(field: Field): Html =
-    Html(s"""<input type="hidden" id="${field.name}" name="${field.name}" value="${field.value.getOrElse("")}">""")
-}
--- src/main/scala/com/micronautics/package.scala
+++ src/main/scala/com/micronautics/package.scala
@@ -5,11 +5,18 @@ import play.api.i18n.{DefaultMessagesApi, Lang, Langs, Messages, MessagesImpl}
 import play.api.mvc.RequestHeader
 
 package object i18n {
-  def bestLang(implicit request: RequestHeader, app: Application, env: Environment, langs: Langs): Lang = {
-    val perfectLang = request.acceptLanguages intersect langs.availables
+  def bestLang(implicit
+               request: RequestHeader,
+               app: Application,
+               env: Environment,
+               langs: Langs
+              ): Lang = {
+    val perfectLang: Seq[Lang] = request.acceptLanguages intersect langs.availables
     perfectLang.headOption.getOrElse {
-      val commonLangCodes = request.acceptLanguages.map(_.language).distinct intersect langs.availables.map(_.language).distinct
-      commonLangCodes.headOption.map(Lang.apply).getOrElse {
+      val requestLangs: Seq[String] = request.acceptLanguages.map(_.language).distinct
+      val availableLangs: Seq[String] = langs.availables.map(_.language).distinct
+      val commonLangs: Seq[String] = requestLangs intersect availableLangs
+      commonLangs.headOption.map(Lang.apply).getOrElse {
         langs.availables.headOption.getOrElse(Lang.defaultLang)
       }
     }
@@ -17,7 +24,13 @@ package object i18n {
 
   /**  Usage: ```implicit val messages = userMessages
     * Messages("key")``` */
-  def userMessages(implicit request: RequestHeader, app: Application, configuration: Configuration, env: Environment, langs: Langs): Messages = {
+  def userMessages(implicit
+                   request: RequestHeader,
+                   app: Application,
+                   configuration: Configuration,
+                   env: Environment,
+                   langs: Langs
+                  ): Messages = {
     lazy val defaultMessages = new DefaultMessagesApi(langs=langs)
     MessagesImpl(bestLang, defaultMessages)
   }
